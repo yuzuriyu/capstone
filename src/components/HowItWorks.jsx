54799726584544227afb3fcd1ddae5f845aa6d6b
cole <garcia.ccole@gmail.com>
2024-02-08 16:30:46 +0800

    save changes

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -10,7 +10,7 @@ const HowItWorks = () => {
           className="w-full h-full object-cover"
         />
         <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 flex flex-col justify-center items-center">
-          <h1 className="text-5xl font-playfair text-white">How It Works</h1>
+          <h1 className="text-5xl font-playfair text-white">Functionality</h1>
         </div>
       </div>
       <div className="w-11/12 m-auto py-20 md:w-10/12 h-[450px]"></div>
