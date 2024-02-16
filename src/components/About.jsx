cc2b0b23006c5a1fe84cf374b920afa1118bdb50
cole <garcia.ccole@gmail.com>
2024-02-16 19:33:07 +0800

    remove hover effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,90 +14,66 @@ export default function About() {
       </div>
       <div className="w-11/12 md:w-9/12 m-auto grid grid-cols-2 gap-4 h-[550px] my-10">
         <div className="grid grid-cols-2 gap-4">
-          <Link
-            to={"/about"}
-            className="rounded-lg overflow-hidden relative group cursor-pointer col-span-2"
-          >
+          <div className="rounded-lg overflow-hidden relative group col-span-2">
             <img
               src="https://i.pinimg.com/564x/53/ee/f5/53eef591108919afa8880e91c9eda0cc.jpg"
               alt=""
-              className="h-full w-full object-cover absolute group-hover:scale-110 transition-transform duration-300 ease-in-out"
+              className="h-full w-full object-cover absolute "
             />
-          </Link>
-          <Link
-            to={"/about"}
-            className="rounded-lg overflow-hidden relative group cursor-pointer col-span-2 "
-          >
+          </div>
+          <div className="rounded-lg overflow-hidden relative group col-span-2 ">
             <img
               src="https://i.pinimg.com/564x/20/96/3f/20963f4566e7e7915002e4a7804831a0.jpg"
               alt=""
-              className="h-full w-full object-cover absolute group-hover:scale-110 transition-transform duration-300 ease-in-out"
+              className="h-full w-full object-cover absolute "
             />
-          </Link>
+          </div>
         </div>
-        <Link
-          to={"/contact"}
-          className="rounded-lg overflow-hidden relative group cursor-pointer"
-        >
+        <div className="rounded-lg overflow-hidden relative group">
           <img
             src="https://i.pinimg.com/564x/ff/24/0c/ff240c08a12b973257d9bd57ed91db5b.jpg"
             alt=""
-            className="h-full w-full object-cover absolute group-hover:scale-110 transition-transform duration-300 ease-in-out"
+            className="h-full w-full object-cover absolute "
           />
-        </Link>
+        </div>
         <div className="grid grid-cols-3 gap-4">
-          <Link
-            to={"/blog"}
-            className="rounded-lg overflow-hidden relative group cursor-pointer"
-          >
+          <div className="rounded-lg overflow-hidden relative group">
             <img
               src="https://i.pinimg.com/564x/a8/50/9e/a8509e24cb8f38fc5a2f9f9b7c06c11f.jpg"
               alt=""
-              className="h-full w-full object-cover absolute group-hover:scale-125 transition-transform duration-300 ease-in-out"
+              className="h-full w-full object-cover absolute "
             />
-          </Link>
-          <Link
-            to={"/blog"}
-            className="rounded-lg overflow-hidden relative group cursor-pointer"
-          >
+          </div>
+          <div className="rounded-lg overflow-hidden relative group">
             <img
               src="https://i.pinimg.com/564x/21/0a/8a/210a8ad1ba131cc1540978cb00073da6.jpg"
               alt=""
-              className="h-full w-full object-cover absolute group-hover:scale-125 transition-transform duration-300 ease-in-out"
+              className="h-full w-full object-cover absolute "
             />
-          </Link>
-          <Link
-            to={"/blog"}
-            className="rounded-lg overflow-hidden relative group cursor-pointer"
-          >
+          </div>
+          <div className="rounded-lg overflow-hidden relative group">
             <img
               src="https://i.pinimg.com/564x/0c/0e/33/0c0e331cbf8bba314af2da0a807c7e9c.jpg"
               alt=""
-              className="h-full w-full object-cover absolute group-hover:scale-125 transition-transform duration-300 ease-in-out"
+              className="h-full w-full object-cover absolute "
             />
-          </Link>
-          <Link
-            to={"/blog"}
-            className="rounded-lg overflow-hidden relative group cursor-pointer row-start-2 col-start-1 col-end-4"
-          >
+          </div>
+          <div className="rounded-lg overflow-hidden relative group row-start-2 col-start-1 col-end-4">
             <img
               src="https://i.pinimg.com/564x/8d/20/d5/8d20d5d0985ffd15f674078e0f7d0bb7.jpg"
               alt=""
-              className="h-full w-full object-cover absolute group-hover:scale-125 transition-transform duration-300 ease-in-out"
+              className="h-full w-full object-cover absolute "
             />
-          </Link>
+          </div>
         </div>
 
-        <Link
-          to={"/blog"}
-          className="rounded-lg overflow-hidden relative group cursor-pointer"
-        >
+        <div className="rounded-lg overflow-hidden relative group">
           <img
             src="https://i.pinimg.com/564x/08/f6/c9/08f6c9711b8de932eaad90cf57fb03ab.jpg"
             alt=""
-            className="h-full w-full object-cover absolute group-hover:scale-125 transition-transform duration-300 ease-in-out"
+            className="h-full w-full object-cover absolute "
           />
-        </Link>
+        </div>
       </div>
     </>
   );
